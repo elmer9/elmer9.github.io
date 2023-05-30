9e4daa39dd2ef01c05ee8661a36508ca7a2a0088
elmeer9 <elmer.winter2@gmail.com>
2023-05-30 17:12:31 +0900

    commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const rectangle = document.getElementById('rectangle');
+const saveButton = document.getElementById('saveButton');
 
 rectangle.onclick = (ev) => {
     const text = document.createElement('span');
@@ -6,10 +7,28 @@ rectangle.onclick = (ev) => {
     text.style.position = 'absolute';
     text.style.left = getRandomPosition(rectangle.offsetWidth - 50) + 'px';
     text.style.top = getRandomPosition(rectangle.offsetHeight - 50) + 'px';
-    text.style.fontSize = 'larger';
+    text.style.fontSize = '40px';
+    text.style.color='red';
     rectangle.appendChild(text);
 };
 
 function getRandomPosition(max) {
     return Math.floor(Math.random() * max);
 }
+
+saveButton.onclick = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = rectangle.offsetWidth;
+    canvas.height = rectangle.offsetHeight;
+  
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(rectangle, 0, 0);
+  
+    const image = canvas.toDataURL('image/png');
+  
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'rectangle_image.png';
+    link.click();
+  };
